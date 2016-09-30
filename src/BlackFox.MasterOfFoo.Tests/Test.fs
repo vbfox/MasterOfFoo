66a61eaff43b024168e45d5faf6a1c7cd91fc9b5
Julien Roncaglia <julien@roncaglia.fr>
2016-09-30 22:40:59 +0200

    Test project

---
+++ src/BlackFox.MasterOfFoo.Tests/Test.fs
@@ -0,0 +1,19 @@
+﻿module BlackFox.MasterOfFoo.DoPrintfTests
+
+open System
+open NUnit.Framework
+open MasterOfFoo.Printf
+
+type TestEnv<'Result>(k, buf) = 
+    inherit PrintfEnv<Text.StringBuilder, unit, 'Result>(buf)
+    override this.Finalize() : 'Result = k ()
+    override this.Write(s : PrintableElement) = ignore(buf.Append(s.ToString()))
+    override this.WriteT(()) = ()
+
+let testprintf (format : StringFormat<'T>)  =
+    doPrintF
+
+[<Test>]
+let TestCase () =
+    Assert.IsTrue(true)
+
