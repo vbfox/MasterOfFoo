a913459433e6a05fe805bccffdb3cef5f776c8cc
Julien Roncaglia <julien@roncaglia.fr>
2016-09-30 22:24:00 +0200

    Paket script

---
+++ paket.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if test "$OS" = "Windows_NT"
+then
+  # use .Net
+
+  .paket/paket.bootstrapper.exe
+  exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+  	exit $exit_code
+  fi
+
+  .paket/paket.exe $@
+else
+  # use mono
+  mono .paket/paket.bootstrapper.exe
+  exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+  	exit $exit_code
+  fi
+
+  mono .paket/paket.exe $@
+fi
