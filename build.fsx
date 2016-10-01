083a5320a8436cc7a9094f6902a674865b4b95e4
Julien Roncaglia <julien@roncaglia.fr>
2016-10-01 22:24:52 +0200

    Better paths

--- build.fsx
+++ build.fsx
@@ -1,12 +1,13 @@
 // include Fake libs
 #r "./packages/FAKE/tools/FakeLib.dll"
-
+#load "./build/TaskDefinitionHelper.fsx"
 open Fake
 
-// Directories
-let buildDir  = "./build/"
-let deployDir = "./deploy/"
-
+let configuration = environVarOrDefault "configuration" "Release"
+let rootDir = __SOURCE_DIRECTORY__
+let artifactsDir = rootDir </> "artifacts"
+let binDir = artifactsDir </> "bin"
+let libraryBinDir = binDir </> "BlackFox.MasterOfFoo"
 
 // Filesets
 let appReferences  =
@@ -18,19 +19,19 @@ let version = "0.1"  // or retrieve from CI server
 
 // Targets
 Target "Clean" (fun _ ->
-    CleanDirs [buildDir; deployDir]
+    CleanDirs [artifactsDir]
 )
 
 Target "Build" (fun _ ->
     // compile all projects below src/app/
-    MSBuildDebug buildDir "Build" appReferences
+    MSBuildDebug null "Build" appReferences
         |> Log "AppBuild-Output: "
 )
 
 Target "Deploy" (fun _ ->
-    !! (buildDir + "/**/*.*")
+    !! (libraryBinDir + "/**/*.*")
         -- "*.zip"
-        |> Zip buildDir (deployDir + "ApplicationName." + version + ".zip")
+        |> Zip libraryBinDir (artifactsDir </> ("ApplicationName." + version + ".zip"))
 )
 
 // Build order
