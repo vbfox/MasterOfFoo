bf7b9d02dbe38e7f7f72617ce3a6fa7bb5a1dab8
Julien Roncaglia <julien@roncaglia.fr>
2016-10-01 18:28:32 +0200

    Some real tests

--- src/BlackFox.MasterOfFoo.Tests/Test.fs
+++ src/BlackFox.MasterOfFoo.Tests/Test.fs
@@ -1,19 +1,32 @@
 ﻿module BlackFox.MasterOfFoo.DoPrintfTests
 
 open System
+open System.Text
 open NUnit.Framework
+open MasterOfFoo
 open MasterOfFoo.Printf
 
-type TestEnv<'Result>(k, buf) = 
-    inherit PrintfEnv<Text.StringBuilder, unit, 'Result>(buf)
-    override this.Finalize() : 'Result = k ()
+type TestEnv() = 
+    inherit PrintfEnv<unit, string, string>()
+    let buf = StringBuilder()
+    override this.Finalize() = buf.ToString ()
     override this.Write(s : PrintableElement) = ignore(buf.Append(s.ToString()))
-    override this.WriteT(()) = ()
+    override this.WriteT(s : string) = ignore(buf.Append(s))
 
-let testprintf (format : StringFormat<'T>)  =
-    doPrintF
+let testprintf (format: Format<'T, unit, string, string>) =
+    doPrintf format (fun _ -> TestEnv() :> PrintfEnv<_, _, _>)
+
+let coreprintf = FSharp.Core.Printf.sprintf
 
 [<Test>]
-let TestCase () =
-    Assert.IsTrue(true)
+let SimpleString () =
+    Assert.AreEqual(
+        testprintf "Foo",
+        coreprintf "Foo")
 
+
+[<Test>]
+let StringFormat () =
+    Assert.AreEqual(
+        testprintf "%s" "Foo",
+        coreprintf "%s" "Foo")
