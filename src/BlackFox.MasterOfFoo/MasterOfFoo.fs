64ba8bd26eb8de48b57da8de803a6f2464324671
Julien Roncaglia <julien@roncaglia.fr>
2016-10-07 22:16:03 +0200

    Add some doc

--- src/BlackFox.MasterOfFoo/MasterOfFoo.fs
+++ src/BlackFox.MasterOfFoo/MasterOfFoo.fs
@@ -5,11 +5,11 @@ module BlackFox.MasterOfFoo.MasterOfFoo
 open System
 open BlackFox.MasterOfFoo.Core
 
-let doPrintf (fmt: Format<'Printer, 'State, 'Residue, 'Result>) (f: int -> #PrintfEnv<'State, 'Residue, 'Result>) = 
-    let formatter, n = PrintfCache.Cache<_, _, _, _>.Get fmt
-    let env() = f(n) :> PrintfEnv<_,_,_>
+let doPrintf (format: Format<'Printer, 'State, 'Residue, 'Result>) (getEnv: int -> #PrintfEnv<'State, 'Residue, 'Result>) = 
+    let formatter, n = PrintfCache.Cache<_, _, _, _>.Get format
+    let env() = getEnv(n) :> PrintfEnv<_,_,_>
     formatter env
 
-let doPrintfFromEnv (fmt: Format<'Printer, 'State, 'Residue, 'Result>) (env: #PrintfEnv<_,_,_>) =
-    let formatter, n = PrintfCache.Cache<_, _, _, _>.Get fmt
+let doPrintfFromEnv (format: Format<'Printer, 'State, 'Residue, 'Result>) (env: #PrintfEnv<_,_,_>) =
+    let formatter, n = PrintfCache.Cache<_, _, _, _>.Get format
     formatter (fun _ -> env :> PrintfEnv<_,_,_>)
--- src/BlackFox.MasterOfFoo/PrintfEnv.fs
+++ src/BlackFox.MasterOfFoo/PrintfEnv.fs
@@ -2,11 +2,22 @@
 
 open System
 
+/// <summary>
 /// Abstracts generated printer from the details of particular environment: how to write text, how to produce results etc...
+/// </summary>
+/// <typeparam name="'State">The state of the printer, passed as argument when using '%t'.</typeparam>
+/// <typeparam name="'Residue">The type that methods passed to '%t' must return.</typeparam>
+/// <typeparam name="'Result">The final result type for the printer.</typeparam>
 [<AbstractClass>]
 type PrintfEnv<'State, 'Residue, 'Result> =
     val State : 'State
     new(s : 'State) = { State = s }
+    
+    /// Create the final result for this printer
     abstract Finalize : unit -> 'Result
+    
+    /// Write an element from the format string (Raw text or format specifier) to the printer
     abstract Write : PrintableElement -> unit
+    
+    /// Write the result of the method provided by %t to the printer.
     abstract WriteT : 'Residue -> unit
