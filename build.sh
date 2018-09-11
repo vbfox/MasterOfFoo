cdb2628ad95c95d7cd98cf26b77dff87b9ced242
Julien Roncaglia <julien@roncaglia.fr>
2018-09-11 21:38:18 +0200

    Small tweaks to vsts linux build

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 ./paket.sh restore || { exit $?; }
 
-dotnet run --project src/BlackFox.MasterOfFoo.Build/BlackFox.MasterOfFoo.Build.fsproj $@
+dotnet run --project src/BlackFox.MasterOfFoo.Build/BlackFox.MasterOfFoo.Build.fsproj -- $@
