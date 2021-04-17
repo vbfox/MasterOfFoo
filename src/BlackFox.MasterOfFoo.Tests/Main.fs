31d9919ea48954feeff3d53578c056be860bb3f6
Julien Roncaglia <julien@roncaglia.fr>
2021-04-17 11:28:31 +0200

    build: Move to net5.0, paket global tool

--- src/BlackFox.MasterOfFoo.Tests/Main.fs
+++ src/BlackFox.MasterOfFoo.Tests/Main.fs
@@ -4,6 +4,6 @@ open Expecto
 
 [<EntryPoint>]
 let main args =
-    let writeResults = TestResults.writeNUnitSummary ("TestResults.xml", "BlackFox.MasterOfFoo.Tests")
+    let writeResults = TestResults.writeNUnitSummary "TestResults.xml"
     let config = defaultConfig.appendSummaryHandler writeResults
     runTestsInAssembly config args
