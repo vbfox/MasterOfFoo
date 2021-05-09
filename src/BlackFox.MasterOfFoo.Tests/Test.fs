7b22e70d6e4899ebf8d4fd2807d48c966f88339d
Julien Roncaglia <julien@roncaglia.fr>
2021-05-10 00:22:25 +0200

    Add a few tests

--- src/BlackFox.MasterOfFoo.Tests/Test.fs
+++ src/BlackFox.MasterOfFoo.Tests/Test.fs
@@ -17,11 +17,21 @@ let coreprintf = FSharp.Core.Printf.sprintf
 
 type Discriminated = |A of string | B of int
 
+let testStr = "Foo"
+let testInt = 42
+
 let tests = [
     test "simple string" {
         Expect.equal
             (coreprintf "Foo")
             (testprintf "Foo")
+            "Foo"
+    }
+
+    test "simple string interpolation" {
+        Expect.equal
+            (coreprintf $"Foo")
+            (testprintf $"Foo")
             "Foo"
     }
 
@@ -43,10 +53,38 @@ let tests = [
             "%5s"
     }
 
+    test "string untyped interpolation" {
+        Expect.equal
+            (coreprintf $"""{"Foo"}""")
+            (testprintf $"""{"Foo"}""")
+            "%s"
+    }
+
+    test "string typed interpolation" {
+        Expect.equal
+            (coreprintf $"""%s{"Foo"}""")
+            (testprintf $"""%s{"Foo"}""")
+            "%s"
+    }
+
     test "int format" {
         Expect.equal
             (coreprintf "%i" 5)
             (testprintf "%i" 5)
+            "%i"
+    }
+
+    test "int untyped interpolation" {
+        Expect.equal
+            (coreprintf $"{5}")
+            (testprintf $"{5}")
+            "%i"
+    }
+
+    test "int typed interpolation" {
+        Expect.equal
+            (coreprintf $"%i{5}")
+            (testprintf $"%i{5}")
             "%i"
     }
 
